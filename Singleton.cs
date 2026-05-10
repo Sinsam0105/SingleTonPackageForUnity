f93f6e4d14505c5074f13bb9bf6027a830125242
jjong22 <jjong13971397@gmail.com>
2026-05-10 11:28:28 +0900

    BaseCommit

---
+++ Singleton.cs
@@ -0,0 +1,21 @@
+public abstract class Singleton<T> where T : class, new()
+{
+    private static T _instance;
+    private static readonly object _lock = new();
+
+    public static T Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                lock (_lock)
+                {
+                    _instance ??= new T();
+                    
+                }
+            }
+            return _instance;
+        }
+    }
+}
---
+++ SingletonBootstrapper.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public static class SingletonBootstrapper
+{
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+    private static void InitializeAutoSingletons()
+    {
+        var registry = Resources.Load<SingletonRegistry>(Constant.Singleton.REGISTRY_RESOURCE_PATH);
+
+        if (registry == null)
+        {
+            if (Constant.Singleton.SHOW_SYSTEM_LOGS)
+            {
+                D.LogError($"[AutoSingleton] Registry not found at Resources/{Constant.Singleton.REGISTRY_RESOURCE_PATH}");
+            }
+            return;
+        }
+
+        foreach (var prefab in registry.prefabs)
+        {
+            if (prefab == null) continue;
+            CreateSingletonInstance(prefab);
+        }
+    }
+
+    private static void CreateSingletonInstance(GameObject prefab)
+    {
+        if (prefab.TryGetComponent<MonoBehaviour>(out var component))
+        {
+            var type = component.GetType();
+
+            if (Object.FindFirstObjectByType(type) == null)
+            {
+                GameObject instance = Object.Instantiate(prefab);
+                instance.name = prefab.name;
+                Object.DontDestroyOnLoad(instance);
+
+                if (Constant.Singleton.SHOW_SYSTEM_LOGS)
+                {
+                    D.Log($"[AutoSingleton] '{prefab.name}' created via Registry.");
+                }
+            }
+        }
+    }
+}
