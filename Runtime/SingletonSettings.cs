70016433ad1798e99a10dc7219995aac06bd7ceb
Sinsam <sejongjsw0105@gmail.com>
2026-05-25 22:24:18 +0900

    Add runtime singleton settings constants

---
+++ Runtime/SingletonSettings.cs
@@ -0,0 +1,9 @@
+namespace Sinsam.SingletonSystem
+{
+    public static class SingletonSettings
+    {
+        public const string PrefabSavePath = "Assets/Resources/AutoSingletons";
+        public const string RegistryResourcePath = "SingletonRegistry";
+        public const string RegistryAssetPath = "Assets/Resources/SingletonRegistry.asset";
+    }
+}
