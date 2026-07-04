fb66dd135398ff3eb765e16758a2b442909fd6f8
Sinsam <sejongjsw0105@gmail.com>
2026-07-05 04:33:54 +0900

    Respect scene rules in MonoSingleton

--- Runtime/MonoSingleton.cs
+++ Runtime/MonoSingleton.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace Sinsam.SingletonSystem
 {
@@ -6,6 +7,7 @@ namespace Sinsam.SingletonSystem
     {
         private static T _instance;
         private static bool _applicationIsQuitting;
+        private static bool _sceneChangeSubscribed;
 
         public static bool HasInstance => _instance != null;
 
@@ -13,9 +15,18 @@ namespace Sinsam.SingletonSystem
         {
             get
             {
+                EnsureSceneChangeSubscribed();
+
                 if (_applicationIsQuitting)
                 {
                     SingletonLogger.Warning($"[MonoSingleton] {typeof(T).Name} was requested while the application is quitting.");
+                    return null;
+                }
+
+                if (!SingletonScenePolicy.IsSceneAllowed(typeof(T)))
+                {
+                    var scene = SceneManager.GetActiveScene();
+                    SingletonLogger.Warning($"[MonoSingleton] {typeof(T).Name} is not registered for scene '{scene.name}'. Instance creation denied.");
                     return null;
                 }
 
@@ -25,8 +36,19 @@ namespace Sinsam.SingletonSystem
 
                     if (_instance == null)
                     {
-                        var singletonObject = new GameObject($"{typeof(T).Name} (Singleton)");
-                        _instance = singletonObject.AddComponent<T>();
+                        var prefab = SingletonScenePolicy.GetPrefab(typeof(T));
+
+                        if (prefab != null)
+                        {
+                            var instanceObject = Instantiate(prefab);
+                            instanceObject.name = prefab.name;
+                            _instance = instanceObject.GetComponent<T>();
+                        }
+                        else
+                        {
+                            var singletonObject = new GameObject($"{typeof(T).Name} (Singleton)");
+                            _instance = singletonObject.AddComponent<T>();
+                        }
                     }
                 }
 
@@ -60,8 +82,19 @@ namespace Sinsam.SingletonSystem
 
         protected virtual void Awake()
         {
+            EnsureSceneChangeSubscribed();
+
             if (_applicationIsQuitting)
             {
+                Destroy(gameObject);
+                return;
+            }
+
+            var scene = SceneManager.GetActiveScene();
+
+            if (!SingletonScenePolicy.IsSceneAllowed(typeof(T), scene))
+            {
+                SingletonLogger.Warning($"[MonoSingleton] {typeof(T).Name} is not registered for scene '{scene.name}'. Destroying object.");
                 Destroy(gameObject);
                 return;
             }
@@ -92,6 +125,39 @@ namespace Sinsam.SingletonSystem
             {
                 _instance = null;
             }
+        }
+
+        private static void EnsureSceneChangeSubscribed()
+        {
+            if (_sceneChangeSubscribed)
+            {
+                return;
+            }
+
+            SceneManager.activeSceneChanged -= HandleActiveSceneChanged;
+            SceneManager.activeSceneChanged += HandleActiveSceneChanged;
+            _sceneChangeSubscribed = true;
+        }
+
+        private static void HandleActiveSceneChanged(Scene oldScene, Scene newScene)
+        {
+            if (_applicationIsQuitting || _instance == null)
+            {
+                return;
+            }
+
+            if (SingletonScenePolicy.IsSceneAllowed(typeof(T), newScene))
+            {
+                return;
+            }
+
+            if (!SingletonScenePolicy.ShouldDestroyWhenSceneNotAllowed(typeof(T)))
+            {
+                return;
+            }
+
+            SingletonLogger.Warning($"[MonoSingleton] {typeof(T).Name} is not registered for scene '{newScene.name}'. DestroyInstance() called.");
+            DestroyInstance();
         }
     }
 }
