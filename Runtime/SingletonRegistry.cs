3f467f4a7a87f493e77d6b374996da2b0cc7937a
Sinsam <sejongjsw0105@gmail.com>
2026-07-05 04:33:23 +0900

    Add scene rules to singleton registry

--- Runtime/SingletonRegistry.cs
+++ Runtime/SingletonRegistry.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace Sinsam.SingletonSystem
 {
@@ -9,7 +11,56 @@ namespace Sinsam.SingletonSystem
         [SerializeField]
         private List<GameObject> prefabs = new();
 
+        [SerializeField]
+        private List<SingletonSceneRule> sceneRules = new();
+
         public IReadOnlyList<GameObject> Prefabs => prefabs;
+        public IReadOnlyList<SingletonSceneRule> SceneRules => sceneRules;
+
+        public bool IsSceneAllowed(Type singletonType, Scene scene)
+        {
+            var rule = FindSceneRule(singletonType);
+            return rule == null || rule.IsSceneAllowed(scene);
+        }
+
+        public bool ShouldDestroyWhenSceneNotAllowed(Type singletonType)
+        {
+            var rule = FindSceneRule(singletonType);
+            return rule != null && rule.DestroyWhenSceneNotAllowed;
+        }
+
+        public GameObject GetPrefab(Type singletonType)
+        {
+            var rule = FindSceneRule(singletonType);
+
+            if (rule?.Prefab != null)
+            {
+                return rule.Prefab;
+            }
+
+            foreach (var prefab in prefabs)
+            {
+                if (prefab != null && prefab.GetComponent(singletonType) != null)
+                {
+                    return prefab;
+                }
+            }
+
+            return null;
+        }
+
+        private SingletonSceneRule FindSceneRule(Type singletonType)
+        {
+            foreach (var rule in sceneRules)
+            {
+                if (rule != null && rule.Matches(singletonType))
+                {
+                    return rule;
+                }
+            }
+
+            return null;
+        }
 
 #if UNITY_EDITOR
         public void SetPrefabs(IEnumerable<GameObject> singletonPrefabs)
@@ -18,5 +69,55 @@ namespace Sinsam.SingletonSystem
             prefabs.AddRange(singletonPrefabs);
         }
 #endif
+    }
+
+    [Serializable]
+    public sealed class SingletonSceneRule
+    {
+        [SerializeField]
+        private GameObject prefab;
+
+        [SerializeField]
+        private bool allowAllScenes = true;
+
+        [SerializeField]
+        private bool destroyWhenSceneNotAllowed = true;
+
+        [SerializeField]
+        private List<string> sceneNames = new();
+
+        public GameObject Prefab => prefab;
+        public bool AllowAllScenes => allowAllScenes;
+        public bool DestroyWhenSceneNotAllowed => destroyWhenSceneNotAllowed;
+        public IReadOnlyList<string> SceneNames => sceneNames;
+
+        public bool Matches(Type singletonType)
+        {
+            return prefab != null && prefab.GetComponent(singletonType) != null;
+        }
+
+        public bool IsSceneAllowed(Scene scene)
+        {
+            if (allowAllScenes)
+            {
+                return true;
+            }
+
+            foreach (var sceneName in sceneNames)
+            {
+                if (string.IsNullOrWhiteSpace(sceneName))
+                {
+                    continue;
+                }
+
+                if (string.Equals(sceneName, scene.name, StringComparison.Ordinal) ||
+                    string.Equals(sceneName, scene.path, StringComparison.Ordinal))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
     }
 }
